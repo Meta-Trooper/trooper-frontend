2a4b6da34493613994f08d9d552a3cc525a93f97
lakshya-dhariwal <lakshyadhariwal9@gmail.com>
2023-02-19 20:55:48 +0530

    styling

--- src/pages/gamers.tsx
+++ src/pages/gamers.tsx
@@ -6,15 +6,75 @@ import { getGamer } from "@trooper/services/gamer";
 const Gamer: NextPage<{ gamers: Gamer[] }> = ({ gamers }) => {
   console.log(Gamer);
   return (
-    <div>
-      {gamers?.map((gamer) => {
-        return (
-          <div className="" key={gamer.id}>
-            <h1>{gamer.username}</h1>
-          </div>
-        );
-      })}
-    </div>
+    <>
+      <h1 className=" mt-16 ml-3 text-highlight text-3xl">Jobs</h1>
+
+      <div className="grid grid-cols-3 mx-auto mt-10 gap-10">
+        {gamers?.map((gamer) => {
+          return (
+            <div
+              className=" p-4 h-full w-full bg-[#1c1c27] rounded-lg flex flex-col justify-between bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-5 border border-highlight cursor-pointer
+          "
+              key={gamer.id}
+            >
+              <div className=" flex justify-between items-center">
+                <h1 className="text-highlight text-2xl">{gamer.username}</h1>{" "}
+                <div className="flex">
+                  <a
+                    href={gamer?.link_twitter}
+                    target={"_blank"}
+                    rel="noreferrer"
+                  >
+                    <img
+                      className="cursor-pointer h-6 mx-1"
+                      src="./twitter.svg"
+                      alt="twitter"
+                    />
+                  </a>{" "}
+                  <a
+                    href={gamer?.link_facebook}
+                    target={"_blank"}
+                    rel="noreferrer"
+                  >
+                    <img
+                      className="cursor-pointer h-6 mx-1"
+                      src="./facebook.svg"
+                      alt="facebook"
+                    />
+                  </a>
+                  <a
+                    href={gamer?.link_linkedin}
+                    target={"_blank"}
+                    rel="noreferrer"
+                  >
+                    <img
+                      className="cursor-pointer h-6 mx-1"
+                      src="./linkedin.svg"
+                      alt="linkedin"
+                    />
+                  </a>
+                </div>
+              </div>
+
+              <h2 className="mt-2 text-lg text-gray-300">
+                {gamer.description}
+              </h2>
+              <div className="flex items-center mt-3 ">
+                <h3 className="flex items-center text-highlight text-2xl">
+                  #{gamer.name_discord || "gamer543"}
+                </h3>
+                <h3 className="ml-4 text-xl text-highlight border border-highlight rounded p-2 px-3">
+                  {gamer.profile_type || 0}
+                </h3>
+              </div>
+              <button className="rounded bg-highlight flex w-full mt-6 text-xl  p-2 justify-center">
+                Hire
+              </button>
+            </div>
+          );
+        })}
+      </div>
+    </>
   );
 };
 
--- src/pages/jobs.tsx
+++ src/pages/jobs.tsx
@@ -6,16 +6,44 @@ import React from "react";
 const Jobs: NextPage<{ jobs: Jobs[] }> = ({ jobs }) => {
   console.log(jobs);
   return (
-    <div>
-      {jobs.length &&
-        jobs?.map((job) => {
-          return (
-            <div className="" key={job.id}>
-              <h1>{job.job_name}</h1>
-            </div>
-          );
-        })}
-    </div>
+    <>
+      <h1 className=" mt-16 ml-3 text-highlight text-3xl">Jobs</h1>
+
+      <div className="grid grid-cols-3 mx-auto mt-10 gap-10">
+        {jobs.length &&
+          jobs?.map((job) => {
+            return (
+              <div
+                className=" p-4 h-full w-full bg-[#1c1c27] rounded-lg flex flex-col justify-between bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-5 border border-highlight cursor-pointer
+                "
+                key={job.id}
+              >
+                <div className=" flex justify-between items-center">
+                  <h1 className="text-highlight text-2xl">{job.job_name}</h1>{" "}
+                  <span className=" border-highlight p-2 rounded border-2 font-semibold   text-highlight">
+                    {job.job_state}
+                  </span>
+                </div>
+
+                <h2 className="mt-2 text-lg text-gray-300">
+                  {job.description}
+                </h2>
+                <div className="flex items-center mt-3 ">
+                  <h3 className="flex items-center text-highlight text-2xl">
+                    {job.payment_amount || 0} ₸
+                  </h3>
+                  <h3 className="ml-4 text-2xl text-subtitle">
+                    {job.duration || 0} Days
+                  </h3>
+                </div>
+                <button className="rounded bg-highlight flex w-full mt-6  p-2 justify-center">
+                  Apply
+                </button>
+              </div>
+            );
+          })}
+      </div>
+    </>
   );
 };
 
